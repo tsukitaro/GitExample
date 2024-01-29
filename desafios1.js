c5c379258452efe517abd325b8a061b99c2f0b83
kert-daiki <dylax01@gmail.com>
2024-01-29 17:59:42 -0500

    Add new javascripts Challenges

---
+++ desafios1.js
@@ -0,0 +1,20 @@
+alert("Bienvenida y bienvenido a nuetro sitio web");
+
+let nombre = "luna";
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+let mensajeDeError = "!Error Completa todos los campos";
+
+alert("!Error Completa todos los campos");
+alert(mensajeDeError);
+
+nombre = prompt("Escriba su nombre de usuario");
+
+edad = prompt("Ingresa tu edad");
+
+if (edad>=18) {
+  alert("Puedes obtener tu licencia de conducir")
+}
+ 
+
---
+++ test.js
@@ -0,0 +1,9 @@
+console.log("hello");
+
+let h = 1;
+
+if (h < 5) {
+  console.log("menor");
+} else {
+  console.log("mayor");
+}
